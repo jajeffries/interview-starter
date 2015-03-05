1840efb96ffa50d3a2cfb661ba37978863082628
developer <James Jeffries>
2015-03-05 09:43:56 +0000

    initial commit

---
+++ java/CheckoutTests.java
@@ -0,0 +1,14 @@
+// import org.junit.Test;
+// import org.junit.TestCase;
+// import static org.junit.Assert.*;
+
+// public class CheckoutTests extends TestCase {
+import junit.framework.TestCase;
+public class CheckoutTests extends TestCase {
+
+	public void testHello() throws Exception {
+		Checkout c = new Checkout();
+		assertTrue(false);
+	}
+	
+}
---
+++ ruby/checkout.rb
@@ -0,0 +1,6 @@
+require 'test/unit'
+
+class CheckoutTests < Test::Unit::TestCase
+
+	def test_something
+		assert_equal true, false
